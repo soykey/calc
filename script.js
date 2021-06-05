92d822559a612d796c8d0a5a72dd536f4dae1fef
tqso <tasogare0715@gmail.com>
2021-06-05 15:54:20 +0900

    add google fonts and functions

---
+++ script.js
@@ -0,0 +1,79 @@
+let hsty = []
+const input = document.getElementById('input')
+const result = document.getElementById('result')
+const formula = document.getElementById('formula')
+const add = (e) =>{
+    input.value += e.value
+}
+const clear = () => {
+    input.value = ''
+}
+const updateres = (value) => {
+    result.innerHTML=value
+}
+const updatefor = (value) => {
+    formula.innerHTML=value
+}
+const calc = () => {
+    const f = new Function('return '+input.value)
+    updateres(f().toString())
+    updatefor(input.value)
+    hsty.push(input.value)
+    clear()
+}
+let count = 0
+let p = 0
+const prsetr = (key) => {
+    if (key == 13) {
+    calc()
+    }
+    if (key == 38 &&count>0) {
+    if (p) {
+        p++
+        count = hsty.length
+    }
+    count--
+    input.value = hsty[count] 
+    }
+    if (key == 40 && count < hsty.length-1) {
+    count++
+    input.value = hsty[count]
+    }
+}
+const del = () => {
+    let inp = input.value.split('')
+    inp.pop()
+    if (inp==[]) {
+    return
+    } else {
+    input.value = inp.join('')
+    }
+}
+const Ans = () => {
+    if (hsty.length==0) {
+    return
+    } else {
+    input.value=`(${hsty.shift()})`
+    }
+}
+const sine = () => {
+    input.value+=`Math.sin(`
+}
+const cosine = () => {
+    input.value+=`Math.cos(`
+}
+const tangent = () => {
+    input.value+=`Math.tan(`
+}
+const pi = () => {
+    input.value+=`Math.PI`
+}
+const pm = () => {
+    let inp = input.value.split('')
+    if (inp[0]!='-') {
+        inp.unshift('-')
+    } else {
+        input.shift()
+    }
+    input.value=inp.join('')
+}
